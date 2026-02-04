6ca638ee984b1a1f288aebe50f84d26356c5d798
Relayced <123452035+Relayced@users.noreply.github.com>
2026-02-04 21:24:26 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   "https://qdvbglwslsjumvqunrob.supabase.co",
-  "sb_publishable_6inzRiKXwFjOE8vSIDOjdw_eCzD2HCl"
+  "sb_publishable_JDcbWNmeTj4STn5STKYoOA_IbebKpuN"
 );
 
 console.log("LIVE supabase URL:", supabase.supabaseUrl);
@@ -10,6 +10,7 @@ console.log("LIVE supabase URL:", supabase.supabaseUrl);
 
 console.log("Supabase URL:", supabase.supabaseUrl);
 console.log("Anon key first 20:", "eyJhbGciOi...".slice(0, 20)); // or your variable if you stored it
+
 
 
 
