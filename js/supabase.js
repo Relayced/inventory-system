8f067a610b08e86b1d8d99cf918a2b2a9d28709e
Relayced <123452035+Relayced@users.noreply.github.com>
2026-02-04 15:55:14 +0800

    Add files via upload

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+const SUPABASE_URL = "https://qdvbglwslsjumvqunrob.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_JDcbWNmeTj4STn5STKYoOA_IbebKpuN";
+
 export const supabase = createClient(
-  "YOUR_PROJECT_URL",
-  "YOUR_PUBLIC_ANON_KEY"
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
