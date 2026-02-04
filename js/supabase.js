7c70e63f344e2d2cc7a4a9434e0262f7da2560e9
Relayced <123452035+Relayced@users.noreply.github.com>
2026-02-04 21:17:13 +0800

    Change Supabase publishable key in supabase.js
    Updated Supabase client initialization with a new publishable key.

--- js/supabase.js
+++ js/supabase.js
@@ -2,11 +2,12 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   "https://qdvbglwslsjumvqunrob.supabase.co",
-  "sb_publishable_JDcbWNmeTj4STn5STKYoOA_IbebKpuN"
+  "sb_publishable_6inzRiKXwFjOE8vSIDOjdw_eCzD2HCl"
 );
 
 console.log("Supabase URL:", supabase.supabaseUrl);
 console.log("Anon key first 20:", "eyJhbGciOi...".slice(0, 20)); // or your variable if you stored it
+
 
 
 
