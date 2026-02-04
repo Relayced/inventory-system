cf68ae1a30e82554bf75f7f9bd613f4bcef44fe2
Relayced <123452035+Relayced@users.noreply.github.com>
2026-02-04 21:08:13 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -5,3 +5,4 @@ export const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkdmJnbHdzbHNqdW12cXVucm9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNzg4ODYsImV4cCI6MjA4NTc1NDg4Nn0.WW9U6puKGl76sdGqGO4jtXtRlOylu5Kn1vjf-W9S47Q"
 );
 
+
