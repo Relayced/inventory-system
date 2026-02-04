bafc0bdbb08a48b2ea0efc56b4b3955e19748cd1
Relayced <123452035+Relayced@users.noreply.github.com>
2026-02-04 21:05:26 +0800

    Change Supabase API key in supabase.js
    Updated Supabase client initialization with a new API key.

--- js/supabase.js
+++ js/supabase.js
@@ -2,5 +2,6 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   "https://qdvbglwslsjumvqunrob.supabase.co",
-  "sb_publishable_JDcbWNmeTj4STn5STKYoOA_IbebKpuN"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkdmJnbHdzbHNqdW12cXVucm9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNzg4ODYsImV4cCI6MjA4NTc1NDg4Nn0.WW9U6puKGl76sdGqGO4jtXtRlOylu5Kn1vjf-W9S47Q"
 );
+
